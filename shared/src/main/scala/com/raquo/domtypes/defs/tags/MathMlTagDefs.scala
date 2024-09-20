fc40d3f31b1ba5d2edd3974ad4e21fddb4214410
Simon Parten <quafadas@gmail.com>
2024-09-20 14:25:44 +0200

    Fixy fixy

--- shared/src/main/scala/com/raquo/domtypes/defs/tags/MathMlTagDefs.scala
+++ shared/src/main/scala/com/raquo/domtypes/defs/tags/MathMlTagDefs.scala
@@ -1,6 +1,6 @@
 package com.raquo.domtypes.defs.attrs
 
-import com.raquo.domtypes.common.{AttrDef, HtmlTagType}
+import com.raquo.domtypes.common.{MathMlTagType, TagDef}
 
 
 object MathMlTagDefs {
