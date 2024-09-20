603064ad28820c5f58e93005ea87b93021f0ffa0
Simon Parten <quafadas@gmail.com>
2024-09-20 13:59:17 +0200

    math tags step1?

---
+++ js/src/test/scala/com/thirdparty/defs/tags/MathTags.scala
@@ -0,0 +1,188 @@
+package com.thirdparty.defs.attrs
+
+import com.thirdparty.keys.HtmlAttr
+import com.thirdparty.codecs._
+
+// #NOTE: GENERATED CODE
+//  - This file is generated at compile time from the data in Scala DOM Types
+//  - See `GeneratorSpec.scala` for code generation params
+//  - Contribute to https://github.com/raquo/scala-dom-types to add missing tags / attrs / props / etc.
+
+/**
+Spec: https://www.w3.org/TR/MathML3/
+
+MDN Docs: https://developer.mozilla.org/en-US/docs/Web/MathML
+https://developer.mozilla.org/en-US/docs/Web/MathML/Element
+*/
+
+trait MathTags {
+
+
+
+  /**
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/math]]
+    */
+  lazy val math: HtmlTag[dom.HTMLElement] = htmlTag("math")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/maligngroup
+    */
+  lazy val maligngroup: HtmlTag[dom.HTMLElement] = htmlTag("maligngroup")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/malignmark
+    */
+  lazy val malignmark: HtmlTag[dom.HTMLElement] = htmlTag("malignmark")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/menclose
+    */
+  lazy val menclose: HtmlTag[dom.HTMLElement] = htmlTag("menclose")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/merror
+    */
+  lazy val merror: HtmlTag[dom.HTMLElement] = htmlTag("merror")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mfenced
+    */
+  lazy val mfenced: HtmlTag[dom.HTMLElement] = htmlTag("mfenced")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mfrac
+    */
+  lazy val mfrac: HtmlTag[dom.HTMLElement] = htmlTag("mfrac")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mglyph
+    */
+  lazy val mglyph: HtmlTag[dom.HTMLElement] = htmlTag("mglyph")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mi
+    */
+  lazy val mi: HtmlTag[dom.HTMLElement] = htmlTag("mi")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mlabeledtr
+    */
+  lazy val mlabeledtr: HtmlTag[dom.HTMLElement] = htmlTag("mlabeledtr")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mlongdiv
+    */
+  lazy val mlongdiv: HtmlTag[dom.HTMLElement] = htmlTag("mlongdiv")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mmultiscripts
+    */
+  lazy val mmultiscripts: HtmlTag[dom.HTMLElement] = htmlTag("mmultiscripts")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mn
+    */
+  lazy val mn: HtmlTag[dom.HTMLElement] = htmlTag("mn")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mo
+    */
+  lazy val mo: HtmlTag[dom.HTMLElement] = htmlTag("mo")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mover
+    */
+  lazy val mover: HtmlTag[dom.HTMLElement] = htmlTag("mover")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mpadded
+    */
+  lazy val mpadded: HtmlTag[dom.HTMLElement] = htmlTag("mpadded")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mphantom
+    */
+  lazy val mphantom: HtmlTag[dom.HTMLElement] = htmlTag("mphantom")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mprescripts
+    */
+  lazy val mprescripts: HtmlTag[dom.HTMLElement] = htmlTag("mprescripts")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mroot
+    */
+  lazy val mroot: HtmlTag[dom.HTMLElement] = htmlTag("mroot")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mrow
+    */
+  lazy val mrow: HtmlTag[dom.HTMLElement] = htmlTag("mrow")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/ms
+    */
+  lazy val ms: HtmlTag[dom.HTMLElement] = htmlTag("ms")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mspace
+    */
+  lazy val mspace: HtmlTag[dom.HTMLElement] = htmlTag("mspace")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/msqrt
+    */
+  lazy val msqrt: HtmlTag[dom.HTMLElement] = htmlTag("msqrt")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mstyle
+    */
+  lazy val mstyle: HtmlTag[dom.HTMLElement] = htmlTag("mstyle")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/msub
+    */
+  lazy val msub: HtmlTag[dom.HTMLElement] = htmlTag("msub")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/msubsup
+    */
+  lazy val msubsup: HtmlTag[dom.HTMLElement] = htmlTag("msubsup")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/msup
+    */
+  lazy val msup: HtmlTag[dom.HTMLElement] = htmlTag("msup")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mtable
+    */
+  lazy val mtable: HtmlTag[dom.HTMLElement] = htmlTag("mtable")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mtd
+    */
+  lazy val mtd: HtmlTag[dom.HTMLElement] = htmlTag("mtd")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mtext
+    */
+  lazy val mtext: HtmlTag[dom.HTMLElement] = htmlTag("mtext")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mtr
+    */
+  lazy val mtr: HtmlTag[dom.HTMLElement] = htmlTag("mtr")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/munder
+    */
+  lazy val munder: HtmlTag[dom.HTMLElement] = htmlTag("munder")
+
+  /**
+    * https://developer.mozilla.org/en-US/docs/Web/MathML/Element/munderover
+    */
+  lazy val munderover: HtmlTag[dom.HTMLElement] = htmlTag("munderover")
+
+
+}
