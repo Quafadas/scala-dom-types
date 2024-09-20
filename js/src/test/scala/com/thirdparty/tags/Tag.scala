bfb318692c2dabcb541755db817ef85a69d4ba8a
Simon Parten <quafadas@gmail.com>
2024-09-20 14:56:21 +0200

    .

--- js/src/test/scala/com/thirdparty/tags/Tag.scala
+++ js/src/test/scala/com/thirdparty/tags/Tag.scala
@@ -15,6 +15,6 @@ case class SvgTag[El <: dom.svg.Element](
   override val domName: String
 ) extends Tag[El]
 
-case class MathTag[El <: dom.svg.Element](
+case class MathTag[El <: dom.html.Element](
   override val domName: String
 ) extends Tag[El]
